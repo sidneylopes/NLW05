4d4a115ad2a2ca8583846cc219127f0ad96dae9d
sidneylopes <sidneylopesjr@gmail.com>
2021-04-23 11:56:50 -0300

    Day 4

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -34,6 +34,25 @@ class SettingsService {
   
   }
 
+  async findByUsername(username: string){
+
+    const settings = await this.settingsRepository.findOne({
+      username
+    });
+    return settings;
+  }
+
+  async update(username: string, chat: boolean){
+    await this.settingsRepository
+      .createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where("username = :username", {
+        username,
+      })
+      .execute();
+  }
+
 }
 
 export { SettingsService };
